edd9c7180131c441b124a70d8d9b82c73865b0e1
Chris Gallup <chris.gallup@hotmail.com>
2016-12-30 13:26:22 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById('tutor').onclick = function () {
+    location.href = "http://abtutoringkc.com/index.php";
+}
+
+document.getElementById('joyful').onclick = function () {
+    location.href = "http://www.joyfulanswers.com/";
+}
