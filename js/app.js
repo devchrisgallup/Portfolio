050b03b53e7819667c12f7eba0557c29883fa331
devchrisgallup <chris.gallup@hotmail.com>
2017-01-27 20:58:39 -0600

    on hover img text box

--- js/app.js
+++ js/app.js
@@ -11,3 +11,48 @@ document.getElementById('joyful').onclick = function () {
 document.getElementById('grocery').onclick = function () {
     location.href = "http://chrisgalluptest.info/angular/index.html";
 }
+
+// on hover chage images
+document.getElementById('tutor').onmouseenter = function () {
+    document.getElementById('tutor').src = 'img/tutortext.png';
+} 
+
+document.getElementById('tutor').onmouseleave = function () {
+    document.getElementById('tutor').src = 'img/tutor.png';
+} 
+
+document.getElementById('joyful').onmouseenter = function () {
+    document.getElementById('joyful').src = 'img/joyfultext.png';
+} 
+
+document.getElementById('joyful').onmouseleave = function () {
+    document.getElementById('joyful').src = 'img/joyful.png';
+} 
+
+document.getElementById('grocery').onmouseenter = function () {
+    document.getElementById('grocery').src = 'img/grocerytext.png';
+} 
+
+document.getElementById('grocery').onmouseleave = function () {
+    document.getElementById('grocery').src = 'img/grocery.png';
+} 
+
+
+// jquery smooth scrolling
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+$('html, body').stop().animate({
+     'scrollTop': $target.offset().top
+}, 900, 'swing');
